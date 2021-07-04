734b3597fbc575ffbc93a7a463983267ebd090fe
Lucas R. Hawthorne Dalbem <52721182+LucasDalben@users.noreply.github.com>
2021-07-04 17:14:31 -0300

    Api de cadastro em banco basica

---
+++ Basicos/CadastroBancario/CadastroBancario/CadastroBancario/ContaBancaria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroBancario {
+    class ContaBancaria {
+        public int NumeroConta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+        
+        public ContaBancaria(int numeroconta, string titular) {
+            NumeroConta = numeroconta;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numeroconta, string titular, double depositoInicial) : this(numeroconta, titular) {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia) {
+            Saldo += quantia;
+        }
+
+        public void Saque(double saque) {
+            Saldo -= saque + 5.0;
+        }
+
+        public override string ToString() {
+            return $"Conta: {NumeroConta}, Titular: {Titular}, Saldo: ${Saldo.ToString("F2")}";
+        }
+    }
+}
---
+++ Basicos/CadastroBancario/CadastroBancario/CadastroBancario/Program.cs
@@ -0,0 +1,38 @@
+﻿ using System;
+
+namespace CadastroBancario {
+    class Program {
+        static void Main(string[] args) {
+
+            ContaBancaria conta;
+
+            Console.Write("Digite o número da Conta: ");
+            int numeroconta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá um depósito inicial (s/n)?");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S') {
+                Console.Write("Digite o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numeroconta, titular, depositoInicial);
+            }
+            else {
+                conta = new ContaBancaria(numeroconta, titular);
+            }
+
+            Console.WriteLine($"\n Dados da Conta: \n {conta}");
+            Console.Write("\n Qual o valor que deseja depositar?");
+            double quantia = double.Parse(Console.ReadLine())
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.Write("\n Qual o valor deseja sacar?");
+            double saque = double.Parse(Console.ReadLine());
+            conta.Saque(saque);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
