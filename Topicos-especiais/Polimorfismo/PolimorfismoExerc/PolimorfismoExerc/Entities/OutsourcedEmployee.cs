5ce8cbedfb4ba4e72a6199e95467937cd8f6b411
Lucas R. Hawthorne Dalbem <52721182+LucasDalben@users.noreply.github.com>
2021-07-23 00:19:55 -0300

    Exercicio de polimorfismo

---
+++ Topicos-especiais/Polimorfismo/PolimorfismoExerc/PolimorfismoExerc/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolimorfismoExerc.Entities {
+    class OutsourcedEmployee : Employee {
+        public double AditionalCharge { get; set; }
+
+        public OutsourcedEmployee() {
+
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double aditionalCharge) : base(name, hours, valuePerHour) {
+            AditionalCharge = aditionalCharge;
+        }
+
+        public override double Payment() {
+            return base.Payment() + 1.1 * AditionalCharge;
+        }
+    }
+}
