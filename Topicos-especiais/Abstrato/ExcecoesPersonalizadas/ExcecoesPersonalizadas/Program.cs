332fb07cd5c4c37d0056b871182dc4721b98b43f
Lucas R. Hawthorne Dalbem <52721182+LucasDalben@users.noreply.github.com>
2021-07-25 21:55:35 -0300

    exerc de exceptions

---
+++ Topicos-especiais/Abstrato/ExcecoesPersonalizadas/ExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ExcecoesPersonalizadas.Entities;
+using ExcecoesPersonalizadas.Entities.Exceptions;
+
+namespace ExcecoesPersonalizadas {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                Console.WriteLine("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine("Check-in date (dd/MM/yyyy: ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine($"Reservation: {reservation}");
+
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy)");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy)");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine($"Reservation: {reservation}");
+            }
+            catch(DomainException e) {
+                Console.WriteLine($"Error in reservation: {e.Message}");
+            }
+            catch(FormatException e) {
+                Console.WriteLine($"Format error: {e.Message}");
+            }
+            catch(Exception e) {
+                Console.WriteLine($"Unexpected error: {e.Message}");
+            }
+            
+        }
+    }
+}
